580b7e1d9234ea4b339ba2dfa1e6155dae9e4e11
estebanmino <efmino@uc.cl>
2018-04-30 15:31:32 -0700

    basic front interaction with contracts

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { safetyContract, web3 } from './EthereumSetup';
+import Safety from './components/Safety';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      coinbase: null,
+      checkedGunAddress: null,
+    }
+
+    this.getCoinbase = this.getCoinbase.bind(this);
+    this.createGun = this.createGun.bind(this);
+    this.checkGun = this.checkGun.bind(this)
+  }
+
+
+  componentWillMount() {
+    this.getCoinbase()
+  }
+  
+
+  getCoinbase() {
+    web3.eth.getCoinbase(function(error, result) {
+      this.setState({ coinbase: result });
+    }.bind(this))
+  }
+
+  checkGun(_make, _model, _serialNumber) {
+    safetyContract.checkGun(
+      _make, _model, _serialNumber,
+      {from: this.state.coinbase},
+      function(error, result) {
+        this.setState({
+          checkedGunAddress: result
+        })
+        console.log("checkGun", result)        
+      }.bind(this)
+    ) 
+  }
+
+  createGun(_make, _model, _serialNumber) {
+    safetyContract.createGun(
+      _make, _model, _serialNumber,
+      {from: this.state.coinbase},
+      function(error, result) {
+        console.log("createGun", result)        
+      }.bind(this)
+    ) 
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">DLT Gun Safety</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Safety 
+          checkGun={this.checkGun}
+          createGun={this.createGun}
+          checkedGunAddress={this.state.checkedGunAddress}
+          />
       </div>
     );
   }
---
+++ front/src/components/Safety.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+
+class Safety extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      createMake: null,
+      createModel: null,
+      createSerialNumber: null,
+      checkMake: null,
+      checkModel: null,
+      checkSerialNumber: null,
+    }
+
+    this.handleCreateGun = this.handleCreateGun.bind(this)
+    this.handleCheckGun = this.handleCheckGun.bind(this)
+    this.handleChangeForm = this.handleChangeForm.bind(this)
+  }
+
+  handleCreateGun(event) {
+    event.preventDefault();
+    this.props.createGun(
+      this.state.createMake,
+      this.state.createModel,
+      this.state.createSerialNumber
+    )
+  }
+
+  handleCheckGun(event) {
+    event.preventDefault();
+    this.props.checkGun(
+      this.state.checkMake,
+      this.state.checkModel,
+      this.state.checkSerialNumber
+    )
+  }
+
+  handleChangeForm(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="create-gun">
+            <form onSubmit={this.handleCreateGun}>
+              <input type="text" placeholder="Make" 
+                name="createMake" value={this.state.createMake || ''} 
+                onChange={this.handleChangeForm} />
+
+              <input type="text" placeholder="Model" 
+                name="createModel" value={this.state.createModel || ''} 
+                onChange={this.handleChangeForm} />
+
+              <input type="text" 
+                placeholder="Serial Number" 
+                name="createSerialNumber" value={this.state.createSerialNumber || ''} 
+                onChange={this.handleChangeForm} />
+              
+              <input type="submit"  value="Create gun" />
+            </form>
+          </div>
+          <div className="check-gun">
+            <form onSubmit={this.handleCheckGun}>
+              <input type="text" placeholder="Make" 
+                name="checkMake" value={this.state.checkMake || ''} 
+                onChange={this.handleChangeForm} />
+
+              <input type="text" placeholder="Model" 
+                name="checkModel" value={this.state.checkModel || ''} 
+                onChange={this.handleChangeForm} />
+
+              <input type="text" 
+                placeholder="Serial Number" 
+                name="checkSerialNumber" value={this.state.checkSerialNumber || ''} 
+                onChange={this.handleChangeForm} />
+              
+              <input type="submit"  value="Check gun" />
+            </form>
+            <p>Gun address: {this.props.checkedGunAddress}</p>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Safety
